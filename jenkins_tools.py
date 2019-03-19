596e170ae7d1235cb71a98ef82f085530d2fbf2c
Khai Phan <khai@emobi.biz>
2019-03-19 20:25:37 +0700

    Update command function

--- jenkins_tools.py
+++ jenkins_tools.py
@@ -1,4 +1,6 @@
 import click
+import jenkins
+import os
 
 @click.group()
 def cli():
@@ -8,26 +10,34 @@ def cli():
 @click.argument('keyword')
 def find(keyword):
     click.echo('List all jobs with name contains a keyword: %s' %keyword)
-    list_job_by_keyword()
+    click.echo(list_job_by_keyword(keyword))
 
 @click.command()
 @click.argument('job_name')
 def build(job_name):
     click.echo('Trigger build for job: %s' %job_name)
-    build_job()
+    build_job(job_name)
 
 cli.add_command(find)
 cli.add_command(build)
 
+JENKINS_HOST_ADDR = "http://18.237.198.223:8080"
 
-jenkins_user = os.environ['JENKINS_USER']
-jenkins_password = os.environ['JENKINS_PASSWORD']
+JENKINS_USER = os.environ['JENKINS_USER']
+JENKINS_PASSWORD = os.environ['JENKINS_PASSWORD']
+server = jenkins.Jenkins(JENKINS_HOST_ADDR, username=JENKINS_USER, password=JENKINS_PASSWORD)
+jobs = server.get_jobs(view_name='All')
 
-def list_job_by_keyword():
-    pass
+def list_job_by_keyword(keyword):
+    list_jobs = []
+    for job in jobs:
+        job_fullname = job['fullname']
+        if keyword in job_fullname:
+            list_jobs.append(job_fullname)
+    return list_jobs
 
-def build_job():
-    pass
+def build_job(job_name):
+    server.build_job(job_name)
 
 if __name__ == '__main__':
     cli()
